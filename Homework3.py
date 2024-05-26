5a9506b68030f0c6860cb20b3aa837c1d25981a4
Дмитрий <dk.ionov@yandex.ru>
2024-05-27 00:58:42 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,29 @@
+name = 'Dmitriy'
+
+print(name)
+
+age = 35
+
+print(age)
+
+age = 35 + 1
+
+print(age)
+
+is_student = True
+
+print(is_student)
+
+name = "Name: " + 'Dmitriy'
+print(name)
+
+age = 35
+age = 'Age: ' + str(age)
+print(age)
+
+age = 35 + 1
+new_age = "New age: " + str(age)
+print(new_age)
+
+is_student = 'Is student: ' + str(is_student)
+print(is_student)
