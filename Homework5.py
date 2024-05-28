ca808c267ab46b9eba08422d5aa59fad63611945
Дмитрий <dk.ionov@yandex.ru>
2024-05-28 21:19:43 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,13 @@
+immutable_var = 7, "Urban", True
+print(immutable_var)
+
+#immutable_var[0] = 9
+#immutable_var[2] = False
+#print(immutable_var)
+
+#  Данные внутри кортежа изменить нелься в отличии от списка
+
+mutable_list = [7, "Urban", True]
+mutable_list[0] = 9
+mutable_list[2] = False
+print(mutable_list)
