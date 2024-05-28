6c939c6c5830c8aaa801f4fccfd8dffcef504ea2
Дмитрий <dk.ionov@yandex.ru>
2024-05-28 20:16:48 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,11 @@
+my_string = input("Введите вашу цитату: ")
+print(len(my_string))
+
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(" ",""))
+print(my_string [0])
+print(my_string [-1])
+
+
+
