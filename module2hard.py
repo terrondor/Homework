5493dd98119f6f8181a50b3ebd52146de9074c52
terrondor <dk.ionov@yandex.ru>
2024-06-10 01:01:26 +0300

    Как-то так.

---
+++ module2hard.py
@@ -0,0 +1,9 @@
+n = int(input("Введите число от 3 до 20: "))
+result = ""
+
+for i in range(1, n):
+    for j in range(i + 1, n + 1):
+        if (i + j) % n == 0:
+            result += str(i) + str(j)
+
+print(result)
