55c8d9c52b802e8d4f1bd407225e13bf8d78e4a7
terrondor <dk.ionov@yandex.ru>
2024-07-03 00:34:31 +0300

    Думаю так

---
+++ module_4_1/module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+result1 = fm(14, 4)
+result2 = fm(7, 0)
+result3 = tm(25, 5)
+result4 = tm(10, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
