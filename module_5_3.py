48efa87d10fdd0293d96c194d4bbab3c4a4eecfd
terrondor <dk.ionov@yandex.ru>
2024-07-09 00:20:48 +0300

    Done

--- module_5_3.py
+++ module_5_3.py
@@ -1,27 +1,50 @@
 class House:
-    def __init__(self, name, numbers_of_floor):
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        if len(args) > 0:
+            name = args[0]
+            cls.houses_history.append(name)
+        return super().__new__(cls)
+
+    def __init__(self, name, floors):
         self.name = name
-        self.numbers_of_floor = numbers_of_floor
+        self.floors = floors
 
-    def go_to(self, new_floor):
-        if new_floor > self.numbers_of_floor or new_floor < 1:
-            print('Такого этажа не существует')
-        else:
-            for floor in range(1, new_floor + 1):
-                print(floor)
-
-    def __len__(self):
-        return self.numbers_of_floor
-
-    def __str__(self):
-        return f'Название:{self.name}, кол-во этажей: {self.numbers_of_floor}'
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
 
     def __eq__(self, other):
-        return self.numbers_of_floor == other.numbers_of_floor
+        if isinstance(other, House):
+            return self.floors == other.floors
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.floors < other.floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.floors <= other.floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.floors > other.floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.floors >= other.floors
+        return NotImplemented
 
     def __add__(self, value):
         if isinstance(value, int):
-            self.numbers_of_floor += value
+            self.floors += value
         return self
 
     def __radd__(self, value):
@@ -30,42 +53,17 @@ class House:
     def __iadd__(self, value):
         return self.__add__(value)
 
-    def __lt__(self, other):
-        return self.numbers_of_floor < other.numbers_of_floor
-
-    def __le__(self, other):
-        return self.numbers_of_floor <= other.numbers_of_floor
-
-    def __gt__(self, other):
-        return self.numbers_of_floor > other.numbers_of_floor
-
-    def __ge__(self, other):
-        return self.numbers_of_floor >= other.numbers_of_floor
-
-    def __ne__(self, other):
-        return self.numbers_of_floor != other.numbers_of_floor
-
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+del h2
+del h3
 
-print(h1 == h2)  # __eq__
+print(House.houses_history)
 
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10  # __iadd__
-print(h1)
-
-h2 = 10 + h2  # __radd__
-print(h2)
-
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+del h1
