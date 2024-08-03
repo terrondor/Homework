a2762d6e4a44c44b76dc37aa27a54c1259121bc7
terrondor <dk.ionov@yandex.ru>
2024-08-03 16:51:22 +0300

    Done

--- module_7_4.py
+++ module_7_4.py
@@ -6,6 +6,8 @@ team1_time = 1552.512
 team2_time = 2153.31451
 tasks_total = 82
 time_avg = 45.2
+challenge_result = 'Победа команды Волшебники данных!'
+
 
 if score1 > score2 or (score1 == score2 and team1_time < team2_time):
     challenge_result = 'Победа команды мастера кода!'
