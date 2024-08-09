3533c4b1f0f0b8124d993b3b6b45a46f1d90e07b
terrondor <dk.ionov@yandex.ru>
2024-08-10 01:07:04 +0300

    Done

--- module_8_1.py
+++ module_8_1.py
@@ -1,20 +1,11 @@
 def add_everything_up(a, b):
     try:
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return a + b
-        elif isinstance(a, str) and isinstance(b, str):
-            return a + b
-        elif (isinstance(a, (int, float)) and isinstance(b, str)) or (
-                isinstance(a, str) and isinstance(b, (int, float))):
-            return str(a) + str(b)
-        else:
-
-            return a + b
-    except:
+        return a + b
+    except Exception:
         return str(a) + str(b)
 
 
-print(add_everything_up(123.456, 'строка'))  # Вывод: '123.456строка'
-print(add_everything_up('яблоко', 4215))  # Вывод: 'яблоко4215'
-print(add_everything_up(123.456, 7))  # Вывод: 130.456
-print(add_everything_up([1, 2], (3, 4)))  # Вывод: '[1, 2](3, 4)'
+# Тестирование функции с предоставленными примерами
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
