9d8b9100b821fe5fe0680d8981fc7842e2de28be
terrondor <dk.ionov@yandex.ru>
2024-09-06 13:17:38 +0300

    Done

---
+++ module_10_5/module_10_5.py
@@ -0,0 +1,32 @@
+import time
+import multiprocessing
+import os
+
+def read_info(name):
+    if not os.path.isfile(name):
+        print(f'Файл не найден: {name}')
+        return
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            all_data.append(line.strip())
+    
+if __name__ == '__main__':
+    filenames = [f'./module_10_5/file {number}.txt' for number in range(1, 5)]
+    
+    print(f"Попытка открыть следующие файлы: {filenames}")
+
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    linear_elapsed_time = time.time() - start_time
+    print(f'{linear_elapsed_time} (линейный)')
+
+    start_time = time.time()
+    with multiprocessing.Pool() as pool:
+        pool.map(read_info, filenames)
+    multi_process_elapsed_time = time.time() - start_time
+    print(f'{multi_process_elapsed_time} (многопроцессный)')
