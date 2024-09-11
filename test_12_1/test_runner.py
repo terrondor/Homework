430dd191f4d7b78b9e559d6295bb8286d7dcbffe
terrondor <dk.ionov@yandex.ru>
2024-09-11 17:47:04 +0300

    Done

---
+++ test_12_1/test_runner.py
@@ -0,0 +1,28 @@
+import unittest
+from runner import Runner
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner('Test Runner')
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+        
+    def test_run(self):
+        runner = Runner('Test Runner')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+        
+    def test_challenge(self):
+        runner_1 = Runner('Test_Runner_1')
+        runner_2 = Runner('Test_Runner_2')
+        for _ in range(10):
+            runner_1.run()
+            runner_2.walk()
+        self.assertNotEqual(runner_1.distance, runner_2.distance)
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
