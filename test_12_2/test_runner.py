cb833b9aa9f0bd71366a4e8a40662eaddebd81f2
terrondor <dk.ionov@yandex.ru>
2024-09-17 22:09:29 +0300

    Done

--- test_12_2/test_runner.py
+++ test_12_2/test_runner.py
@@ -15,25 +15,25 @@ class TournamentTest(unittest.TestCase):
     def tearDownClass(cls):
         for key in sorted(cls.all_results.keys()):
             results = cls.all_results[key]
-            # Print result in desired format
+
             print({pos: runner.name for pos, runner in results.items()})
 
     def test_race_1(self):
         tournament = Tournament(90, self.runner_1, self.runner_3)
         results = tournament.start()
-        self.all_results[1] = results  # Store results with race number
+        self.all_results[1] = results
         self.assertTrue(results[1].name == "Усэйн", "Expected winner is Усэйн")
 
     def test_race_2(self):
         tournament = Tournament(90, self.runner_2, self.runner_3)
         results = tournament.start()
-        self.all_results[2] = results  # Store results with race number
+        self.all_results[2] = results
         self.assertTrue(results[1].name == "Андрей", "Expected winner is Андрей")
 
     def test_race_3(self):
         tournament = Tournament(90, self.runner_1, self.runner_2, self.runner_3)
         results = tournament.start()
-        self.all_results[3] = results  # Store results with race number
+        self.all_results[3] = results
         self.assertTrue(results[1].name == "Усэйн", "Expected winner is Усэйн")
 
 if __name__ == "__main__":
