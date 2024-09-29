9c3cf58000f16a613a930c693b28654dc5d32848
Дмитрий <dk.ionov@yande.ru>
2024-09-29 21:59:11 +0300

    Файл логирования

---
+++ module_13_6.py
@@ -0,0 +1,86 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+api = "7426305553:AAEGpI0EYLblIEfa3ZmhsjI6whniCvC7468"
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text='Расчитать')
+button_2 = KeyboardButton(text='Информация')
+kb.add(button).add(button_2)
+
+inline_kb = InlineKeyboardMarkup()
+inline_button_calories = InlineKeyboardButton(text='Расчитать норму калорий', callback_data='calories')
+inline_button_formulas = InlineKeyboardButton(text='Формула расчета', callback_data='formulas')
+inline_kb.add(inline_button_calories, inline_button_formulas)
+
+
+@dp.message_handler(commands=['start'])
+async def main_menu(message: types.Message):
+    await message.answer('Привет! Я посчитаю норму ваших калорий в день. Выберите опцию.', reply_markup=kb)
+
+
+@dp.message_handler(text='Расчитать')
+async def calculate_menu(message: types.Message):
+    await message.answer("Выберите опцию:", reply_markup=inline_kb)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call: types.CallbackQuery):
+    await call.answer()
+    formula_text = ("Формула Миффлина-Сан Жеора:\n"
+                    "Для мужчин: BMR = 10 * вес(кг) + 6.25 * рост(см) - 5 * возраст(лет) + 5\n"
+                    "Для женщин: BMR = 10 * вес(кг) + 6.25 * рост(см) - 5 * возраст(лет) - 161")
+    await call.message.answer(formula_text)
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call: types.CallbackQuery):
+    await call.answer()
+    await call.message.answer("Введите ваш возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await UserState.growth.set()
+    await message.answer("Введите ваш рост в сантиметрах:")
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await UserState.weight.set()
+    await message.answer("Введите ваш вес в килограммах:")
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data["age"])
+    growth = int(data["growth"])
+    weight = int(data["weight"])
+
+    bmr = 10 * weight + 6.25 * growth - 5 * age + 5
+    await message.reply(f"Ваша норма калорий: {bmr} ккал в день.")
+
+    await state.finish()
+
+
+if __name__=="__main__":
+    executor.start_polling(dp, skip_updates=True)
