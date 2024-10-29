6bcbf787623a62a1f4bbf39ffc22ab1a90eca996
Дмитрий <dk.ionov@yande.ru>
2024-10-29 20:54:00 +0300

    Forget validation.

--- module_16/module_16_3.py
+++ module_16/module_16_3.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 
 app = FastAPI()
@@ -12,14 +13,17 @@ async def get_all_messages() -> dict:
 
 
 @app.post("/user/{username}/{age}")
-async def create_user(username: str, age: int) -> str:
+async def create_user(username: Annotated[str, Path(min_length=1, max_length=15, description="Enter user name:")],
+                      age: Annotated[int, Path(ge=1, le=100, description="Enter user age:")]) -> str:
     new_id = str(max(map(int, users.keys())) + 1)
     users[new_id] = f'Имя {username}, Возраст: {age}'
     return f"User {new_id} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_message(user_id: str, username: str, age: int) -> str:
+async def update_message(user_id: Annotated[int, Path(ge=1, le=100, description="Enter user name:")],
+                         username: Annotated[str, Path(min_length=1, max_length=15, description="Enter user name:")],
+                         age: Annotated[int, Path(ge=1, le=100, description="Enter user age:")]) -> str:
     users[user_id] = f"Имя: {username}, возраст: {age}"
     if user_id in users:
         return f"User {user_id} is updated"
