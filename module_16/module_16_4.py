69c754b0c980393f1963b125bbd812fbb301ee36
Дмитрий <dk.ionov@yande.ru>
2024-11-11 08:49:37 +0300

    Made changes.

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -1,12 +1,7 @@
-
-
-from fastapi import FastAPI, Path, status, HTTPException
-from typing import Annotated
-
-from fastapi.openapi.utils import status_code_ranges
+from fastapi import FastAPI, Path, HTTPException
+from fastapi import status
 from pydantic import BaseModel
 from typing import List
-
 
 app = FastAPI()
 
@@ -19,41 +14,34 @@ class User(BaseModel):
     age: int
 
 
-@app.get("/users")
+@app.get("/users", response_model=List[User])
 def get_all_users() -> List[User]:
     return users
 
 
-@app.post("/user/{username}/{age}")
-def create_user(username: Annotated[str, Path(min_length=1, max_length=15, description="Enter user name:")],
-                      age: Annotated[int, Path(ge=1, le=100, description="Enter user age:")]) -> str:
-    if users:
-        user_id = max(users, key=lambda u: u.id).id + 1
-    else:
-        user_id = 0
-    users.append(User(id=user_id, username=username, age=age))
-    return f"User {user_id} is created"
+@app.post("/users", response_model=User, status_code=status.HTTP_201_CREATED)
+def create_user(username: str, age: int) -> User:
+    user_id = len(users) + 1
+    new_user = User(id=user_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
 
 
+@app.put("/users/{user_id}", response_model=User)
+def update_user(user_id: int, username: str, age: int) -> User:
+    if user_id < 1 or user_id > len(users):
+        raise HTTPException(status_code=404, detail="User not found")
 
-@app.put("/user/{user_id}/{username}/{age}")
-def update_message(user_id: Annotated[int, Path(ge=1, le=100, description="Enter user name:")],
-                         username: Annotated[str, Path(min_length=1, max_length=15, description="Enter user name:")],
-                         age: Annotated[int, Path(ge=1, le=100, description="Enter user age:")]) -> str:
-    try:
-        edit_user = users[user_id]
-        edit_user.username = username
-        edit_user.age = age
-        return f"User {user_id} is updated"
-    except HTTPException:
-        raise HTTPException(status_code=404, detail="User {user_id} is not")
+    edit_user = users[user_id - 1]
+    edit_user.username = username
+    edit_user.age = age
+    return edit_user
 
 
+@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(users):
+        raise HTTPException(status_code=404, detail="User not found")
 
-@app.delete("/user/{user_id}")
-def delete_user(user_id: int) -> str:
-    try:
-        users.pop(user_id)
-        return f"User {user_id} is deleted"
-    except HTTPException:
-        raise HTTPException(status_code=404, detail="User {user_id} is not"
+    users.pop(user_id - 1)
+    return
